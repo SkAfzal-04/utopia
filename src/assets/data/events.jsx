ed54149e0764687041cb5f6388712fc1571a9a19
Arup Maity <138391665+Arup8@users.noreply.github.com>
2025-01-28 18:20:08 +0530

    Update events.jsx

--- src/assets/data/events.jsx
+++ src/assets/data/events.jsx
@@ -2,129 +2,136 @@
 
 export const eventsByDay = {
   1: [
-    {
-      time: '1:30 PM - 1:40 PM',
-      title: 'Ganesh Vandana and Lighting of Lamp',
-      location: '',
-      description: ''
-    },
-    {
-      time: '1:40 PM - 1:50 PM',
-      title: 'Inaugural Dance',
-      location: '',
-      description: ''
-    },
-    {
-      time: '1:50 PM - 1:55 PM',
-      title: 'Welcoming Dignitaries onto the Dias',
-      location: '',
-      description: ''
-    },
-    {
-      time: '1:55 PM - 2:00 PM',
-      title: 'Welcome address by Mr. Kishan Kumar Kejriwal, Chairman MCKV Group',
-      location: '',
-      description: ''
-    },
-    {
-      time: '2:00 PM - 2:05 PM',
-      title: 'Address by Prof. (Dr.) Partha Sarathi Chakraborty, CEO MCKV Group',
-      location: '',
-      description: ''
-    },
-    {
-      time: '2:05 PM - 2:10 PM',
-      title: 'Address by Prof. (Dr.) Abhijit Lahiri, Principal MCKVIE',
-      location: '',
-      description: ''
-    },
-    {
-      time: '2:10 PM - 2:20 PM',
-      title: 'Felicitation of a Nurse and Civic Volunteer for their best service to the society. Launching of UTOPIA-2k25 T-Shirt',
-      location: '',
-      description: ''
-    },
-    {
-      time: '2:20 PM - 2:25 PM',
-      title: 'Vote of Thanks by Prof. AK Jalan, Chairman UTOPIA-2k25',
-      location: '',
-      description: ''
-    },
-    {
-      time: '2:30 PM',
-      title: 'End of Inaugural Session',
-      location: '',
-      description: ''
-    },
-    {
-      time: '2:35 PM - 4:30 PM',
-      title: 'Declaration of OCTET (singing competition) prize winners and Singing performances by students with orchestra',
-      location: '',
-      description: ''
-    },
-    {
-      time: '4:45 PM - 6:45 PM',
-      title: 'Dance and College Band Performances',
-      location: '',
-      description: ''
-    },
-    {
-      time: '7:00 PM - 9:00 PM',
-      title: 'Guest Band Performance',
-      location: '',
-      description: ''
-    }
-  ],
+  {
+    "time": "1:30 PM - 1:40 PM",
+    "title": "Ganesh Vandana and Lighting of Lamp",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "1:40 PM - 1:50 PM",
+    "title": "Inaugural Dance",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "1:50 PM - 1:55 PM",
+    "title": "Welcoming Dignitaries onto the Dias",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "1:55 PM - 2:00 PM",
+    "title": "Welcome Address by Mr. Kishan Kumar Kejriwal, Chairman MCKV Group",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "2:00 PM - 2:05 PM",
+    "title": "Address by Prof. (Dr.) Partha Sarathi Chakraborthy, CEO MCKV Group",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "2:05 PM - 2:10 PM",
+    "title": "Address by Prof. (Dr.) Abhijit Lahiri, Principal MCKVIE",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "2:10 PM - 2:15 PM",
+    "title": "Address by Chief Guest, Mr. Sagar Khastagir, Alumni 2003 POB",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "2:15 PM - 2:20 PM",
+    "title": "Felicitation of a Nurse and Civic Volunteer for their Best Service to the Society",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "2:20 PM - 2:25 PM",
+    "title": "Launching of UTOPIA-2k25 T-Shirt",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "2:25 PM - 2:30 PM",
+    "title": "Vote of Thanks by Prof. (Dr.) A. K Jalan, Chairman UTOPIA-2K25",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "2:35 PM",
+    "title": "End of Inaugural Session",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "2:40 PM - 4:35 PM",
+    "title": "Singing Performances (under OCTET) by Students with Orchestra/Track",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "4:35 PM - 6:40 PM",
+    "title": "Dance Performances",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "6:50 PM - 8:50 PM",
+    "title": "Singing Performance by Guest Artist - Ankita Bhattacharyya",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "8:55 PM - 9:15 PM",
+    "title": "DJ Performance",
+    "location": "",
+    "description": ""
+  }
+],
   
   2: [
-    {
-      time: '2:30 PM - 3:00 PM',
-      title: 'Performance by Heritage Club of MCKVIE',
-      location: '',
-      description: ''
-    },
-    {
-      time: '3:05 PM - 4:30 PM',
-      title: 'Singing Performances by OCTET prize winners and MCKVIE students',
-      location: '',
-      description: ''
-    },
-    {
-      time: '4:35 PM - 6:00 PM',
-      title: 'Dance Performances',
-      location: '',
-      description: ''
-    },
-    {
-      time: '6:05 PM - 6:45 PM',
-      title: 'Fashion Show - Style Symphony and Performance',
-      location: '',
-      description: ''
-    },
-    {
-      time: '6:50 PM - 7:20 PM',
-      title: 'Performance by UTOPIA-2k25 Student Committee Members',
-      location: '',
-      description: ''
-    },
-    {
-      time: '7:40 PM - 9:00 PM',
-      title: 'Singing Performance by Guest Artist',
-      location: '',
-      description: ''
-    },
-    {
-      time: '9:05 PM - 9:25 PM',
-      title: 'DJ Performance',
-      location: '',
-      description: ''
-    },
-    {
-      time: '9:30 PM',
-      title: 'End of the Program',
-      location: '',
-      description: ''
-    }
-  ]
+  {
+    "time": "3:00 PM - 4:30 PM",
+    "title": "Band Performances by MCKVIE Students",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "4:35 PM - 6:00 PM",
+    "title": "Dance Performances",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "6:05 PM - 6:45 PM",
+    "title": "Fashion Show - The Fashion Runway and Performance",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "6:50 PM - 7:20 PM",
+    "title": "Performance by UTOPIA-2k25 Student Committee Members",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "7:40 PM - 9:00 PM",
+    "title": "Guest Band Performance - Bullet and Bohemians",
+    "location": "",
+    "description": ""
+  },
+  {
+    "time": "9:05 PM",
+    "title": "End of the Program",
+    "location": "",
+    "description": ""
+  }
+]
+
   
 };
